71e6115585cf07586c30e085f3617a1a72a82316
pau13-loop <pllinas@cifpfbmoll.eu>
2022-03-31 22:30:10 +0200

    build: :heavy_plus_sign: Add webpack dependency and configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,29 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/main.js',
     output: {
         path: `${__dirname}/dist`,
         filename: 'bundle.js'
-    }
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader'
+            }
+        ]
+    },
+    mode: 'development'
 }
