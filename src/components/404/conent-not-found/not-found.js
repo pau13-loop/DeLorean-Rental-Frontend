6a03951cddc4f5584b54ca78da483061a70a71ee
pau13-loop <pllinas@cifpfbmoll.eu>
2022-04-02 21:28:09 +0200

    feat: :sparkles: Create component NotFound 404

---
+++ src/components/404/conent-not-found/not-found.js
@@ -0,0 +1,10 @@
+import './not-found.css';
+import notFoundContent from './not-found.html';
+
+export default () => {
+    const notFoundContainer = document.createElement('div');
+    notFoundContainer.innerHTML = notFoundContent;
+
+    return notFoundContainer;
+
+}
