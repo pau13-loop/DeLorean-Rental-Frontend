6f34565c5a31e4628c87be2ce028fee2056eb8c2
pau13-loop <pllinas@cifpfbmoll.eu>
2022-04-02 21:29:37 +0200

    feat: :building_construction: Change architecture components

--- src/views/stock/stock.js
+++
@@ -1,9 +0,0 @@
-import { stockComponents } from "../../components/stock";
-
-export default () => {
-    const stockView = document.createElement('div');
-    stockView.classList.add('contentStock');
-    stockView.appendChild(stockComponents.homeBtnRow());
-
-    return stockView;
-}
